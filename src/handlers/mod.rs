2c61ea96edc29c9e4542150ca46b322bce13d2f4
Harry Bairstow <h.bairstow22@gmail.com>
2022-08-09 11:43:30 +0100

    Client registration

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -2,3 +2,33 @@ pub mod health;
 pub mod register_client;
 pub mod delete_client;
 pub mod push_message;
+
+#[derive(serde::Serialize)]
+pub struct ErrorReason {
+    pub field: String,
+    pub description: String,
+}
+
+#[derive(serde::Serialize)]
+pub struct ErrorResponse {
+    pub status: String,
+    pub reasons: Vec<ErrorReason>
+}
+
+pub fn new_error_response(reasons: Vec<ErrorReason>) -> ErrorResponse {
+    ErrorResponse {
+        status: "FAILED".to_string(),
+        reasons
+    }
+}
+
+#[derive(serde::Serialize)]
+pub struct SuccessResponse {
+    status: String
+}
+
+pub fn new_success_response() -> SuccessResponse {
+    SuccessResponse {
+        status: "OK".to_string()
+    }
+}
--- src/handlers/register_client.rs
+++ src/handlers/register_client.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
+use redis::Commands;
 use warp::http;
 use crate::State;
 use serde::Deserialize;
+use crate::handlers::{ErrorReason, new_error_response, new_success_response};
 
 #[derive(Deserialize)]
 pub struct RegisterBody {
@@ -11,11 +13,77 @@ pub struct RegisterBody {
     pub token: String
 }
 
-pub async fn handler(_state: Arc<State>, mut _body: RegisterBody) -> Result<impl warp::Reply, warp::Rejection> {
-    let response = warp::reply::with_status(
-        "Register Client",
-        http::StatusCode::OK
-    );
+pub async fn handler(state: Arc<State>, body: RegisterBody) -> Result<impl warp::Reply, warp::Rejection> {
+    let internal_server_error = new_error_response(vec![]);
 
-    Ok(response)
+    if !vec!["fcm", "apns"].contains(&&*body.push_type.to_lowercase()) {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(
+                &new_error_response(vec![
+                    ErrorReason {
+                        field: "type".to_string(),
+                        description: "Invalid Push Service, must be one of: fcm, apns".to_string()
+                    }
+                ])
+            ),
+            http::StatusCode::BAD_REQUEST)
+        );
+    }
+
+    if body.token.is_empty() {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(
+                &new_error_response(vec![
+                    ErrorReason {
+                        field: "token".to_string(),
+                        description: "The `token` field must not be empty".to_string()
+                    }
+                ])
+            ),
+            http::StatusCode::BAD_REQUEST)
+        );
+    }
+
+    let redis = state.get_redis_connection();
+    if let Err(_) = redis {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&internal_server_error),
+            http::StatusCode::INTERNAL_SERVER_ERROR
+        ));
+    }
+    let mut redis_conn = redis.unwrap();
+
+    let exists = redis_conn.exists::<&String, bool>(&body.client_id);
+    if let Err(_) = exists {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&internal_server_error),
+            http::StatusCode::INTERNAL_SERVER_ERROR
+        ));
+    }
+
+    if exists.unwrap() == true {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(
+                &new_error_response(vec![
+                    ErrorReason {
+                        field: "client_id".to_string(),
+                        description: "A client is already registered with this id".to_string()
+                    }
+                ])
+            ),
+            http::StatusCode::BAD_REQUEST)
+        );
+    }
+
+    if let Err(_) = redis_conn.set::<&String, String, ()>(&body.client_id, format!("{}:{}", body.push_type, body.token)) {
+        return Ok(warp::reply::with_status(
+            warp::reply::json(&internal_server_error),
+            http::StatusCode::INTERNAL_SERVER_ERROR
+        ));
+    }
+
+    Ok(warp::reply::with_status(
+        warp::reply::json(&new_success_response()),
+        http::StatusCode::OK
+    ))
 }
