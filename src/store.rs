2285f756685a8222e4b42aea97690e4d492537a2
Harry Bairstow <h.bairstow22@gmail.com>
2022-08-14 06:32:59 +0100

    Generalize ClientStore and HashMap impl

--- src/store.rs
+++ src/store.rs
@@ -7,25 +7,25 @@ pub struct Client {
     pub token: String,
 }
 
-pub trait ClientStore {
-    fn create_client(&mut self, id: String, client: Client) -> error::Result<()>;
-    fn get_client(&self, id: String) -> error::Result<Option<&Client>>;
-    fn delete_client(&mut self, id: String) -> error::Result<()>;
+pub trait ClientStore<K> {
+    fn create_client(&mut self, id: K, client: Client) -> error::Result<()>;
+    fn get_client(&self, id: &K) -> error::Result<Option<&Client>>;
+    fn delete_client(&mut self, id: &K) -> error::Result<()>;
 }
 
-impl ClientStore for HashMap<String, Client> {
-    fn create_client(&mut self, id: String, client: Client) -> error::Result<()> {
+impl<K> ClientStore<K> for HashMap<K, Client> {
+    fn create_client(&mut self, id: K, client: Client) -> error::Result<()> {
         self.insert(id, client);
         Ok(())
     }
 
-    fn get_client(&self, id: String) -> error::Result<Option<&Client>> {
-        let client = self.get(&*id);
+    fn get_client(&self, id: &K) -> error::Result<Option<&Client>> {
+        let client = self.get(id);
         Ok(client)
     }
 
-    fn delete_client(&mut self, id: String) -> error::Result<()> {
-        self.remove(&*id);
+    fn delete_client(&mut self, id: &K) -> error::Result<()> {
+        self.remove(id);
         Ok(())
     }
 }
