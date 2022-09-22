33a889c38dfaad480681d6e9bba350d3d2b9c2dd
Harry Bairstow <h.bairstow22@gmail.com>
2022-09-22 15:13:19 +0100

    Create VPC

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,30 @@ data "assert_test" "workspace" {
   throw = "default workspace is not valid in this project"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "${terraform.workspace}-${local.app_name}"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = var.azs
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  private_subnet_tags = {
+    Visibility = "private"
+  }
+  public_subnet_tags = {
+    Visibility = "public"
+  }
+
+  enable_dns_support     = true
+  enable_dns_hostnames   = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+}
+
 module "tags" {
   source = "github.com/WalletConnect/terraform-modules/modules/tags"
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,17 @@
+variable "region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "azs" {
+  type = list(string)
+  default = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
+
 variable "public_url" {
+  type = string
+}
+
+variable "grafana_endpoint" {
   type = string
 }
