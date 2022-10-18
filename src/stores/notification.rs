a9c942d18d074edb625465af747e83e06dc9ef92
Harry Bairstow <h.bairstow22@gmail.com>
2022-10-18 21:49:53 +0100

    feat: Insert or Update notification

--- src/stores/notification.rs
+++ src/stores/notification.rs
@@ -1,9 +1,9 @@
 use crate::error;
+use crate::handlers::push_message::MessagePayload;
 use async_trait::async_trait;
 use chrono::{DateTime, Utc};
-use sqlx::Executor;
 use sqlx::types::Json;
-use crate::handlers::push_message::MessagePayload;
+use sqlx::Executor;
 
 #[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
 pub struct Notification {
@@ -14,7 +14,7 @@ pub struct Notification {
     previous_payloads: Vec<Json<MessagePayload>>,
 
     last_received_at: DateTime<Utc>,
-    created_at: DateTime<Utc>
+    created_at: DateTime<Utc>,
 }
 
 #[async_trait]
@@ -22,8 +22,9 @@ pub trait NotificationStore {
     async fn create_or_update_notification(
         &self,
         id: &str,
-        client: Notification,
-    ) -> error::Result<()>;
+        client_id: &str,
+        payload: MessagePayload,
+    ) -> error::Result<Notification>;
     async fn get_notification(&self, id: &str) -> error::Result<Option<Notification>>;
     async fn delete_notification(&self, id: &str) -> error::Result<()>;
 }
@@ -32,19 +33,38 @@ pub trait NotificationStore {
 impl NotificationStore for sqlx::PgPool {
     async fn create_or_update_notification(
         &self,
-        _id: &str,
-        _client: Notification,
-    ) -> error::Result<()> {
-        todo!()
+        id: &str,
+        client_id: &str,
+        payload: MessagePayload,
+    ) -> error::Result<Notification> {
+        let res = sqlx::query_as::<sqlx::postgres::Postgres, Notification>(
+            "INSERT INTO public.notifications (id, client_id, last_payload)
+VALUES ($1, $2, $3)
+ON CONFLICT (id)
+    DO UPDATE SET last_payload      = $3,
+                  previous_payloads = array_append(previous_payloads, $3),
+                  last_received_at  = now()
+RETURNING *;",
+        )
+        .bind(id)
+        .bind(client_id)
+        .bind(payload)
+        .fetch_one(self)
+        .await;
+
+        match res {
+            Err(e) => Err(e.into()),
+            Ok(row) => Ok(row),
+        }
     }
 
     async fn get_notification(&self, id: &str) -> error::Result<Option<Notification>> {
         let res = sqlx::query_as::<sqlx::postgres::Postgres, Notification>(
             "SELECT * FROM public.notifications WHERE id = $1",
         )
-            .bind(id)
-            .fetch_one(self)
-            .await;
+        .bind(id)
+        .fetch_one(self)
+        .await;
 
         match res {
             Err(sqlx::Error::RowNotFound) => Ok(None),
@@ -54,7 +74,8 @@ impl NotificationStore for sqlx::PgPool {
     }
 
     async fn delete_notification(&self, id: &str) -> error::Result<()> {
-        let mut query_builder = sqlx::QueryBuilder::new("DELETE FROM public.notifications WHERE id = ");
+        let mut query_builder =
+            sqlx::QueryBuilder::new("DELETE FROM public.notifications WHERE id = ");
         query_builder.push_bind(id);
         let query = query_builder.build();
 
