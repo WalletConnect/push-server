689e9d71b8b9f2e1d9be6f10c904723f9ce411da
Harry Bairstow <h.bairstow22@gmail.com>
2022-11-10 14:55:54 +0000

    fix(infra): Typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 locals {
   app_name = "push"
   fqdn     = terraform.workspace == "prod" ? var.public_url : "${terraform.workspace}.${var.public_url}"
-  latest_release_name = github_release.latest_release.name
+  latest_release_name = data.github_release.latest_release.name
   version = coalesce(var.image_version, substr(local.latest_release_name, 1, length(local.latest_release_name)))
 }
 
