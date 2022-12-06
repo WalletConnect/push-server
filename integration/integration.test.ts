20f495cd58716a63f3df2dd135c4b75ac84f13a4
Derek <alexanderderekrein@gmail.com>
2022-12-06 17:58:40 -0500

    feat: move files

--- integration/integration.test.ts
+++ integration/integration.test.ts
@@ -11,6 +11,8 @@ const BASE_URLS = new Map<string, string>([
   ['dev', 'http://localhost:3000'],
 ])
 
+const TEST_TENANT = 'f1e0fcb9-75f8-49b5-a05b-00c35ac8418f'
+
 const BASE_URL = BASE_URLS.get(process.env.JEST_ENV)
 
 describe('Echo Server', () => {
@@ -19,6 +21,27 @@ describe('Echo Server', () => {
 
     it('is healthy', async () => {
       const { status } = await axios.get(`${url}`)
+
+      expect(status).toBe(200)
+    })
+  })
+  describe('Client Registration', () => {
+    const url = `${BASE_URL}/${TEST_TENANT}/clients`
+
+    it('registers a client', async () => {
+      const { status, data } = await axios.post(
+        `${url}`,
+        {
+          client_id: Math.random().toString(36).substr(2, 5),
+          type: 'apns',
+          token: Math.random().toString(36).substr(2, 5),
+        },
+        {
+          headers: {
+            'content-type': 'application/json',
+          },
+        },
+      )
 
       expect(status).toBe(200)
     })
