8d9e7cc0fe7b11cf62eb56ee9e3c1a152c61df4f
Harry Bairstow <me@harryet.xyz>
2023-02-20 11:58:58 +0000

    chore: fmt

--- tests/functional/stores/tenant.rs
+++ tests/functional/stores/tenant.rs
@@ -28,7 +28,10 @@ async fn tenant_creation(ctx: &mut StoreContext) {
 async fn tenant_deletion(ctx: &mut StoreContext) {
     let id = Uuid::new_v4().to_string();
 
-    let res = ctx.tenants.create_tenant(TenantUpdateParams { id: id.clone() }).await;
+    let res = ctx
+        .tenants
+        .create_tenant(TenantUpdateParams { id: id.clone() })
+        .await;
 
     assert!(res.is_ok());
 
