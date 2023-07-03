0be5086e2fb0f2d618855195604cda16886017a7
Xavier Basty <xavier@walletconnect.com>
2023-07-03 23:51:05 +0200

    feat: add `msg_id` to analytics exports (#156)

--- src/analytics/message_info.rs
+++ src/analytics/message_info.rs
@@ -3,6 +3,7 @@ use {parquet_derive::ParquetRecordWriter, serde::Serialize, std::sync::Arc};
 #[derive(Debug, Clone, Serialize, ParquetRecordWriter)]
 #[serde(rename_all = "camelCase")]
 pub struct MessageInfo {
+    pub msg_id: Arc<str>,
     pub region: Option<Arc<str>>,
     pub country: Option<Arc<str>>,
     pub continent: Option<Arc<str>>,
