88416fbc2ca82a526a0d75f14bfda1561a246b85
Rakowskiii <38994251+Rakowskiii@users.noreply.github.com>
2023-07-12 09:28:06 +0200

    feat: move analytics to proper s3 bucket (#159)

--- terraform/analytics/outputs.tf
+++ terraform/analytics/outputs.tf
@@ -3,11 +3,6 @@ output "bucket-arn" {
   value       = aws_s3_bucket.analytics-data-lake_bucket.arn
 }
 
-output "bucket-name" {
-  description = "The name of the analytics bucket."
-  value       = aws_s3_bucket.analytics-data-lake_bucket.bucket
-}
-
 output "kms-key_arn" {
   description = "The ARN of the KMS key used to encrypt the analytics bucket."
   value       = aws_kms_key.analytics_bucket.arn
