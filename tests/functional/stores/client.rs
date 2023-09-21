9666874a8cd79ef48f245e1c311a7ff09f209102
Max Kalashnikoff <geekmaks@gmail.com>
2023-09-21 22:35:38 +0300

    Revert "updating `client_upsert` functional test to reflect query changes"
    This reverts commit f7b4ebdad5dea89a6217c993f612ae67b95017d1.

--- tests/functional/stores/client.rs
+++ tests/functional/stores/client.rs
@@ -56,77 +56,30 @@ async fn client_creation_apns(ctx: &mut StoreContext) {
 
 #[test_context(StoreContext)]
 #[tokio::test]
-async fn client_upsert_token(ctx: &mut StoreContext) {
+async fn client_upsert(ctx: &mut StoreContext) {
     let id = gen_id();
 
-    // Initial Client creation
-    ctx.clients
-        .create_client(TENANT_ID, &id, Client {
-            tenant_id: TENANT_ID.to_string(),
-            push_type: ProviderKind::Fcm,
-            token: TOKEN.to_string(),
-        })
-        .await
-        .unwrap();
-    let insert_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
-    assert_eq!(insert_result.token, TOKEN);
-
-    // Updating token for the same id
-    let updated_token = gen_id();
-    ctx.clients
+    let res = ctx
+        .clients
         .create_client(TENANT_ID, &id, Client {
             tenant_id: TENANT_ID.to_string(),
             push_type: ProviderKind::Apns,
-            token: updated_token.clone(),
+            token: TOKEN.to_string(),
         })
-        .await
-        .unwrap();
-    let updated_token_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
-    assert_eq!(updated_token_result.token, updated_token);
+        .await;
 
-    // Cleaning up records
-    ctx.clients.delete_client(TENANT_ID, &id).await.unwrap();
-}
+    assert!(res.is_ok());
 
-#[test_context(StoreContext)]
-#[tokio::test]
-async fn client_upsert_id(ctx: &mut StoreContext) {
-    let id = gen_id();
-
-    // Initial Client creation
-    ctx.clients
+    let upsert_res = ctx
+        .clients
         .create_client(TENANT_ID, &id, Client {
             tenant_id: TENANT_ID.to_string(),
             push_type: ProviderKind::Fcm,
             token: TOKEN.to_string(),
         })
-        .await
-        .unwrap();
-    let insert_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
-    assert_eq!(insert_result.token, TOKEN);
+        .await;
 
-    // Updating id for the same token
-    let updated_id = gen_id();
-    ctx.clients
-        .create_client(TENANT_ID, &updated_id, Client {
-            tenant_id: TENANT_ID.to_string(),
-            push_type: ProviderKind::Fcm,
-            token: TOKEN.to_string(),
-        })
-        .await
-        .unwrap();
-    let updated_id_result = ctx
-        .clients
-        .get_client(TENANT_ID, &updated_id)
-        .await
-        .unwrap();
-    assert_eq!(updated_id_result.token, TOKEN);
-
-    // Cleaning up records
-    ctx.clients
-        .delete_client(TENANT_ID, &updated_id)
-        .await
-        .unwrap();
+    assert!(upsert_res.is_ok());
 }
 
 #[test_context(StoreContext)]
