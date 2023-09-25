3f8f9f6cc9f0871184f7818a91e1639478138714
Max Kalashnikoff <geekmaks@gmail.com>
2023-09-25 15:14:37 +0300

    adding notifications for the new client in tests to check constraint

--- tests/functional/stores/client.rs
+++ tests/functional/stores/client.rs
@@ -61,55 +61,108 @@ async fn client_creation_apns(ctx: &mut StoreContext) {
 #[test_context(StoreContext)]
 #[tokio::test]
 async fn client_upsert_token(ctx: &mut StoreContext) {
-    let id = format!("id-{}", gen_id());
+    let client_id = format!("id-{}", gen_id());
     let token = format!("token-{}", gen_id());
 
     // Initial Client creation
     ctx.clients
-        .create_client(TENANT_ID, &id, Client {
+        .create_client(TENANT_ID, &client_id, Client {
             tenant_id: TENANT_ID.to_string(),
             push_type: ProviderKind::Fcm,
             token: token.clone(),
         })
         .await
         .unwrap();
-    let insert_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
+    let insert_result = ctx.clients.get_client(TENANT_ID, &client_id).await.unwrap();
     assert_eq!(insert_result.token, token);
+
+    // Insert notification for the client to test the clients->notifications
+    // constraint works properly
+    let notification_id = format!("id-{}", gen_id());
+    let notification_payload = format!("payload-{}", gen_id());
+
+    ctx.notifications
+        .create_or_update_notification(
+            &notification_id,
+            TENANT_ID,
+            &client_id,
+            &echo_server::handlers::push_message::MessagePayload {
+                topic: String::new(),
+                flags: 0,
+                blob: notification_payload,
+            },
+        )
+        .await
+        .unwrap();
+    let get_notification_result = ctx
+        .notifications
+        .get_notification(&notification_id, TENANT_ID)
+        .await
+        .unwrap();
+    assert_eq!(get_notification_result.client_id, client_id);
 
     // Updating token for the same id
     let updated_token = format!("token-{}", gen_id());
     ctx.clients
-        .create_client(TENANT_ID, &id, Client {
+        .create_client(TENANT_ID, &client_id, Client {
             tenant_id: TENANT_ID.to_string(),
             push_type: ProviderKind::Apns,
             token: updated_token.clone(),
         })
         .await
         .unwrap();
-    let updated_token_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
+    let updated_token_result = ctx.clients.get_client(TENANT_ID, &client_id).await.unwrap();
     assert_eq!(updated_token_result.token, updated_token);
 
     // Cleaning up records
-    ctx.clients.delete_client(TENANT_ID, &id).await.unwrap();
+    ctx.clients
+        .delete_client(TENANT_ID, &client_id)
+        .await
+        .unwrap();
 }
 
 #[test_context(StoreContext)]
 #[tokio::test]
 async fn client_upsert_id(ctx: &mut StoreContext) {
-    let id = format!("id-{}", gen_id());
+    let client_id = format!("id-{}", gen_id());
     let token = format!("token-{}", gen_id());
 
     // Initial Client creation
     ctx.clients
-        .create_client(TENANT_ID, &id, Client {
+        .create_client(TENANT_ID, &client_id, Client {
             tenant_id: TENANT_ID.to_string(),
             push_type: ProviderKind::Fcm,
             token: token.clone(),
         })
         .await
         .unwrap();
-    let insert_result = ctx.clients.get_client(TENANT_ID, &id).await.unwrap();
+    let insert_result = ctx.clients.get_client(TENANT_ID, &client_id).await.unwrap();
     assert_eq!(insert_result.token, token.clone());
+
+    // Insert notification for the client to test the clients->notifications
+    // constraint works properly
+    let notification_id = format!("id-{}", gen_id());
+    let notification_payload = format!("payload-{}", gen_id());
+
+    ctx.notifications
+        .create_or_update_notification(
+            &notification_id,
+            TENANT_ID,
+            &client_id,
+            &echo_server::handlers::push_message::MessagePayload {
+                topic: String::new(),
+                flags: 0,
+                blob: notification_payload,
+            },
+        )
+        .await
+        .unwrap();
+    let get_notification_result = ctx
+        .notifications
+        .get_notification(&notification_id, TENANT_ID)
+        .await
+        .unwrap();
+    assert_eq!(get_notification_result.client_id, client_id);
 
     // Updating id for the same token
     let updated_id = gen_id();
