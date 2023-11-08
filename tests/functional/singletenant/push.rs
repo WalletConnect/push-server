daf21dd724e8b0abb0e12f13a75c85386aed1a17
Chris Smith <chris@walletconnect.com>
2023-11-08 10:07:15 -0500

    chore: break tests

--- tests/functional/singletenant/push.rs
+++ tests/functional/singletenant/push.rs
@@ -37,7 +37,7 @@ async fn create_client(ctx: &mut EchoServerContext) -> (ClientId, String, MockSe
         let mock_server = MockServer::start().await;
         Mock::given(method(Method::Get))
             .respond_with(ResponseTemplate::new(StatusCode::OK))
-            .expect(1..1)
+            .expect(0..0)
             .mount(&mock_server)
             .await;
         mock_server
