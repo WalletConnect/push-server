fc3825421e1d75bb27e1824d551b730337b491ac
Max Kalashnikoff | maksy.eth <geekmaks@gmail.com>
2024-06-21 10:00:52 +0200

    fix(apns): proper handling of the wrong CA in APNS certificate (#343)

--- src/handlers/push_message.rs
+++ src/handlers/push_message.rs
@@ -435,6 +435,23 @@ pub async fn handler_internal(
                     );
                     Err(Error::TenantSuspended)
                 }
+                Error::ApnsCertificateUnknownCA => {
+                    let reason = "Unknown APNs certificate's CA";
+                    state
+                        .tenant_store
+                        .suspend_tenant(&tenant_id, reason)
+                        .await
+                        .map_err(|e| (e, analytics.clone()))?;
+                    increment_counter!(state.metrics, tenant_suspensions);
+                    warn!(
+                        %tenant_id,
+                        client_id = %client_id,
+                        notification_id = %notification.id,
+                        push_type = client.push_type.as_str(),
+                        "tenant has been suspended due to: {reason}"
+                    );
+                    Err(Error::TenantSuspended)
+                }
                 Error::BadFcmApiKey => {
                     state
                         .tenant_store
